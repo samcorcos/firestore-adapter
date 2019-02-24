27616d7e2680be98622f3441ab186391d3c9567e
xxxx <sam@corcos.io>
2019-02-24 12:07:47 -0500

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import Adapter from './adapter';
-import { data } from './resource';
+// import Adapter from './adapter';
+// import { data } from './resource';
 
-const adapter = new Adapter(data);
-console.log(JSON.stringify(adapter.convert()));
+// const adapter = new Adapter(data);
+// console.log(JSON.stringify(adapter.convert()));
+
+import Adapter from './adapter'
+
+export {
+  Adapter
+}
+
+export default Adapter
